42ffa66314f6145294d83ee3791c481867122d36
rwxce <148581386+rwxce@users.noreply.github.com>
2025-02-08 10:17:09 +0000

    add new table and fix some issues

--- tgtnotes_schema.sql
+++ tgtnotes_schema.sql
@@ -54,9 +54,9 @@ CREATE TABLE activity (
 	type NVARCHAR(20) CHECK (type IN ('event', 'task', 'reminder')),
 	description NVARCHAR(255),
 	date DATE,
-	admin_id INT,
+	admin_user_id INT,
 	CONSTRAINT pk_activity PRIMARY KEY (id),
-	CONSTRAINT fk_activity_admin FOREIGN KEY(admin_id) REFERENCES admin (id)
+	CONSTRAINT fk_activity_admin FOREIGN KEY(admin_user_id) REFERENCES admin (id)
 );
 
 CREATE TABLE app (
@@ -65,7 +65,7 @@ CREATE TABLE app (
 	mail NVARCHAR(255),
 	password NVARCHAR(255),
 	role_id INT,
-	rating FLOAT,
+	rating INT CHECK (rating BETWEEN 1 AND 5),
 	latitude DECIMAL(10,8),
 	longitude DECIMAL(11,8),
 	active BIT,
@@ -99,22 +99,46 @@ CREATE TABLE messages (
 	CONSTRAINT fk_messages_chat FOREIGN KEY(chat_id) REFERENCES chats (id)
 );
 
+CREATE TABLE contracts (
+	id INT IDENTITY(1,1),
+	artist_id INT,
+	space_id INT,
+	init_hour TIME,
+	end_hour TIME,
+	CONSTRAINT pk_contracts PRIMARY KEY (id),
+	CONSTRAINT uq_contract UNIQUE (artist_id, space_id)
+	CONSTRAINT fk_contracts_artist FOREIGN KEY(artist_id) REFERENCES app (id),
+	CONSTRAINT fk_contracts_space FOREIGN KEY(space_id) REFERENCES app (id)
+);
+
+CREATE TABLE rating (
+    id INT IDENTITY(1,1) PRIMARY KEY,
+	rating INT CHECK (rating BETWEEN 1 AND 5),
+    artist_id INT NULL,
+    space_id INT NULL,
+    contract_id INT NULL,
+    CONSTRAINT fk_rating_app FOREIGN KEY(artist_id) REFERENCES app(id),
+    CONSTRAINT fk_rating_space FOREIGN KEY(space_id) REFERENCES app(id),
+    CONSTRAINT fk_rating_contract FOREIGN KEY(contract_id) REFERENCES contracts(id)
+);
+
 CREATE TABLE spaces (
-	app_id INT,
+	app_user_id INT,
+	capacity INT,
 	zip_code NVARCHAR(10),
-	CONSTRAINT pk_spaces PRIMARY KEY (app_id),
-	CONSTRAINT fk_spaces_app FOREIGN KEY(app_id) REFERENCES app (id)
+	CONSTRAINT pk_spaces PRIMARY KEY (app_user_id),
+	CONSTRAINT fk_spaces_app FOREIGN KEY(app_user_id) REFERENCES app (id)
 );
 
 CREATE TABLE artists (
-	app_id INT,
+	app_user_id INT,
 	genre_id INT,
-	CONSTRAINT pk_artists PRIMARY KEY (app_id, genre_id),
-	CONSTRAINT fk_artists_app FOREIGN KEY(app_id) REFERENCES app (id),
+	CONSTRAINT pk_artists PRIMARY KEY (app_user_id, genre_id),
+	CONSTRAINT fk_artists_app FOREIGN KEY(app_user_id) REFERENCES app (id),
 	CONSTRAINT fk_artists_genre FOREIGN KEY(genre_id) REFERENCES genres (id)
 );
 
-CREATE TABLE match_events (
+CREATE TABLE matches (
 	artist_id INT,
 	space_id INT,
 	CONSTRAINT pk_match PRIMARY KEY (artist_id, space_id),
@@ -122,25 +146,13 @@ CREATE TABLE match_events (
 	CONSTRAINT fk_match_space FOREIGN KEY(space_id) REFERENCES app (id)
 );
 
-CREATE TABLE contracts (
-	artist_id INT,
-	space_id INT,
-	init_hour TIME,
-	end_hour TIME,
-	musician_rating FLOAT,
-	restaurant_rating FLOAT,
-	CONSTRAINT pk_contract PRIMARY KEY (artist_id, space_id),
-	CONSTRAINT fk_contract_artist FOREIGN KEY(artist_id) REFERENCES app (id),
-	CONSTRAINT fk_contract_space FOREIGN KEY(space_id) REFERENCES app (id)
-);
-
 CREATE TABLE incidences (
-	app_id INT,
-	admin_id INT,
+	app_user_id INT,
+	admin_user_id INT,
 	description NVARCHAR(MAX),
 	status NVARCHAR(20) CHECK (status IN ('open', 'in_progress', 'closed')),
-	CONSTRAINT pk_incidences PRIMARY KEY (app_id, admin_id),
-	CONSTRAINT fk_incidences_app FOREIGN KEY(app_id) REFERENCES app (id),
-	CONSTRAINT fk_incidences_admin FOREIGN KEY(admin_id) REFERENCES admin (id)
+	CONSTRAINT pk_incidences PRIMARY KEY (app_user_id, admin_user_id),
+	CONSTRAINT fk_incidences_app FOREIGN KEY(app_user_id) REFERENCES app (id),
+	CONSTRAINT fk_incidences_admin FOREIGN KEY(admin_user_id) REFERENCES admin (id)
 );
 GO
